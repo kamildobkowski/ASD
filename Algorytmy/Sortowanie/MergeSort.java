4485dd7f6c210eabb23595ccd54fa64db7b7ff9e
kamildobkowski <kamil.dobkowski.03@gmail.com>
2023-10-17 12:27:59 +0200

    Naprawilem merge sorta

--- Algorytmy/Sortowanie/MergeSort.java
+++ Algorytmy/Sortowanie/MergeSort.java
@@ -1,31 +1,31 @@
 package Algorytmy.Sortowanie;
 
 public class MergeSort {
-	public void mergeSort(double[] v, int n) {
-		double [] tmp = new double[n];
-		double [] s = v;
-		double [] d = tmp;
+	private void mergeSort(double[] nums) {
+		int n = nums.length;
+		double[] tmp = new double[n];
+		double[] s = nums;
+		double[] d = tmp;
 
 		int l = 2;
 		while(l/2 < n) {
 			for(int ss = 0; ss < n; ss+=l) {
-				merge(s, d, ss, (ss+l/2 > n ? n : ss+1), (ss+l > n ? n : ss+1));
+				merge(s, d, ss, (Math.min(ss + l / 2, n)), (Math.min(ss + l, n)));
 			}
 			tmp = s;
 			s = d;
 			d = tmp;
 			l*=2;
 		}
-		if(s != v) {
-			System.arraycopy(v, 0, s, 0, n);
+		if(s != nums) {
+			System.arraycopy(nums, 0, s, 0, n);
 			s = null;
 		}
 		else {
 			d = null;
 		}
 	}
-
-	private void merge(double []s, double []d, int ps, int ss, int ks) {
+	private void merge(double[] s, double[] d, int ps, int ss, int ks) {
 		int i = ps;
 		int j = ss;
 		int k = ps;
@@ -38,11 +38,17 @@ public class MergeSort {
 				d[k++] = s[j++];
 			}
 		}
-		while(i<ss) {
-			d[k++] = s[i++];
+
+		while (i<ss) {
+			d[k]=s[i];
+			k++;
+			i++;
 		}
 		while(j<ks) {
-			d[k++] = s[j++];
+			d[k]=s[j];
+			k++;
+			j++;
 		}
 	}
+
 }
